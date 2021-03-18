e6a0987b3954982c62cb3e051e08189df330462e
Pritam Samanta <pritamsamanta47@gmail.com>
2021-03-18 12:53:55 +0000

    Adding servo motor test file

---
+++ test/Arduino/stepper motor.ino
@@ -0,0 +1,22 @@
+/*
+* Hardware Connection
+* Servo red wire – 5V pin Arduino         
+* Servo brown wire – Ground pin Arduino         
+* Servo yellow wire – PWM(9) pin Arduino
+*/
+
+#include <Servo.h> //Including header file to operate servo motor
+
+   Servo myservo; //Creating a object of servo class
+   int val;
+
+void setup() {
+   myservo.attach(9); //Connection servo motor to arduino
+}
+
+void loop() {
+   for(val = 0; val < 180; val++){
+        myservo.write(val); //Rotating servo motor from 0 to 180 dergree
+        delay(15);
+   }
+}
