35a0a45ea4a4c3964a2c07f2b106cc458537453a
Pritam Samanta <pritamsamanta47@gmail.com>
2021-03-18 13:14:25 +0000

    Fixed formatting

--- test/Arduino/stepper motor.ino
+++ test/Arduino/stepper motor.ino
@@ -7,16 +7,19 @@
 
 #include <Servo.h> //Including header file to operate servo motor
 
-   Servo myservo; //Creating a object of servo class
-   int val;
+Servo myservo; //Creating a object of servo class
+int val;
 
-void setup() {
-   myservo.attach(9); //Connection servo motor to arduino
+void setup()
+{
+    myservo.attach(9); //Connection servo motor to arduino
 }
 
-void loop() {
-   for(val = 0; val < 180; val++){
+void loop()
+{
+    for (val = 0; val < 180; val++)
+    {
         myservo.write(val); //Rotating servo motor from 0 to 180 dergree
         delay(15);
-   }
+    }
 }
